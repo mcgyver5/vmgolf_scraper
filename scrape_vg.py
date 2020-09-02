dafc5241e49a546f5efdc302aecf25d8c65bfcd2
mcgyver5 <tim.e.mcguire@gmail.com>
2020-09-02 03:58:27 +0000

    fix boolean of if user has done this problem, fix date, loop pages

--- scrape_vg.py
+++ scrape_vg.py
@@ -32,14 +32,16 @@ def is_done(vimgolf_user):
 
     return url_set
 
-def dig_date(some_html):
-    notice_divs = soup.find_all("div", {"class": "notice clearfix"})
+def dig_date(soup_bone):
+    notice_divs = soup_bone.find_all("div", {"class": "notice clearfix"})
     winner = notice_divs[0]
     date_em = winner.find_all("em")
     challenge_date = date_em[0].text
     date_parts = challenge_date.split(" ")
-    print(date_parts[0])
-    return date_parts[0]
+    date_string = date_parts[0]
+    parts = date_string.split("/")
+    form_date = parts[2] + "-" + parts[0] + "-" + parts[1]
+    return form_date
 
 def dig_winner(winner):
     anchors = winner.find_all("a")
@@ -47,7 +49,6 @@ def dig_winner(winner):
     return user
 
 def dig_creator(some_html):
-    print("dig_creator")
     h5_tags = soup.find_all("h5")
     for h5 in h5_tags:
         if "Created by: " in h5:
@@ -61,7 +62,6 @@ def dig_min(soup):
     return score_tag.text
     
 def dig_max(some_html):
-    print("dig_max")
     success_divs = soup.find_all("div", {"class":"success clearfix"})
     nonwinner = success_divs[-1]
     b_tags = nonwinner.find_all("b")
@@ -85,67 +85,86 @@ def get_headers():
     return headers
 def dig_entries(some_html):
     stats = soup.findAll("b", {"class": "stat"})
-    for b in stats:
-        print(b)
-    print(stats[0])
-    print(stats[1])
     golfers = stats[0].text
     entries = stats[1].text
-    print(entries)
     return entries
 
+def get_href_dict(soup):
+    
+    href_dict = {}
+    h5_tags = soup.find_all('h5')
+    for h5_tag in h5_tags:
+        anchors = h5_tag.find_all('a')
+        first_anchor = anchors[0]
+        link_text = first_anchor.text
+        link_address = first_anchor['href']
+        hash12 = link_address[12:]
+        href_dict[hash12] = link_text
+    return href_dict
+
+def get_pages(start_url, soup):
+    page_list = []
+    pages = soup.find_all("span", {"class": "page"})
+    for page in pages:
+        anchor = page.find('a')
+        if anchor == None:
+            print("none")
+        else:
+            page_url = anchor['href']
+            page_list.append(start_url + page_url)
+    return page_list
+
+def capture_challenges(soup):
+
+    try:
+        conn = sqlite3.connect("vimgolf.db")
+        c = conn.cursor()
+        href_dict = get_href_dict(soup)
+        done = 0
+        for h in href_dict:
+            full_url = "https://www.vimgolf.com/challenges/" + h
+            resp = requests.get(full_url, headers=req_headers)
+            time.sleep(0.05)
+            soup1 = BeautifulSoup(resp.content, 'html.parser')
+            if h in finished_challs:
+                print("{} is in finished challenges".format(h))
+                done = 1
+            notice_divs = soup1.find_all("div", {"class": "notice clearfix"})
+            winner = notice_divs[0]
+            entries = dig_entries(soup1)
+            winner = dig_winner(winner)
+            creator = dig_creator(soup1)
+            title = href_dict[h]
+            min_keys = dig_min(soup1)
+        #    max_keys = dig_max(soup)
+            max_keys = 100
+            ch_date = dig_date(soup1)
+            sql = "insert into vg_challenges values (:h, :entries, :create_date, :winner, :min_keys, :max_keys, :creator, :title, :done)"
+            c.execute(sql, {'h':h, 'entries':entries, 'create_date':ch_date,'winner':winner,'min_keys':min_keys, 'max_keys':max_keys, 'creator':creator, 'title':title, 'done':done})
+            conn.commit()
+            done = 0
+        c.close()
+        conn.close()
+    except Exception as e:
+        print("error")
+        print(e)
+                                                             
+                                                                                    
 start_url = "https://www.vimgolf.com"
 finished_challs = is_done("mcguire")
-
-for f in finished_challs:
-    print(f)
 req_headers = get_headers()
-
 response = requests.get(start_url, headers=req_headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
-href_dict = {}
-h5_tags = soup.find_all('h5')
-for h5_tag in h5_tags:
-    anchors = h5_tag.find_all('a')
-    first_anchor = anchors[0]
-    link_text = first_anchor.text
-    link_address = first_anchor['href']
-    hash12 = link_address[12:]
-    href_dict[hash12] = link_text
+capture_challenges(soup)
+all_pages = get_pages(start_url,soup)
 count = 0
-try:
-    conn = sqlite3.connect("vimgolf.db")
-    c = conn.cursor()
-
-    for h in href_dict.keys():
-        full_url = "https://www.vimgolf.com/challenges/" + h
-        resp = requests.get(full_url, headers=req_headers)
-        time.sleep(0.05)
-        soup = BeautifulSoup(resp.content, 'html.parser')
-
-        notice_divs = soup.find_all("div", {"class": "notice clearfix"})
-        winner = notice_divs[0]
-
-        entries = dig_entries(soup)
-        winner = dig_winner(winner)
-        creator = dig_creator(soup)
-        title = href_dict[h]
-        min_keys = dig_min(soup)
-        max_keys = dig_max(soup)
-        ch_date = dig_date(winner)
-        count = count + 1
-
-        sql = "insert into vg_challenges values (:h, :entries, :create_date, :winner, :min_keys, :max_keys, :creator, :title)"
-        c.execute(sql, {'h':h, 'entries':entries, 'create_date':ch_date,'winner':winner,'min_keys':min_keys, 'max_keys':max_keys, 'creator':creator, 'title':title})
-
-        if count > 10:
-            break
-    conn.commit()
-    c.close()
-    conn.close()
-except Exception as e:
-    print("error")
-    print(e)
-                                                             
-                                                                                    
+done = 0
+for page_url in all_pages:
+    count = count + 1
+    print(page_url)
+    response = requests.get(page_url, headers=req_headers)
+    soup = BeautifulSoup(response.content, 'html.parser') 
+    capture_challenges(soup)
+    if count > 2:
+        break
